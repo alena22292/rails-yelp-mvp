a11e533272305bfee8b277476b14a3b1876d7264
Alena Gritsiuk <alena22292@mail.ru>
2020-11-18 14:17:27 +0100

    Build an app with two models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,9 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
-  # validates :phone_number, presence: true
+  validates :phone_number, presence: true
   validates :address, presence: true
   validates :category, presence: true, inclusion: {
-    in: ["chinese", "italian", "japanese", "french", "belgian"]
+    in: %w[chinese italian japanese french belgian]
   }
   has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:update, :edit, :destroy] do
+    # Nested!
+    resources :reviews, only: [:new, :create]
+  end
+    # collection do
+    #   get 'top'
+    # end
+
+    # member do
+    #   get 'chef'
+    # end
+
+  # get 'restaurants/top', to: 'restaurants#top', as: :top_restaurants
+  # get 'restaurants/:id/chef', to: 'restaurants#chef', as: :chef_restaurant
+
+  resources :reviews, only: :destroy
 end
