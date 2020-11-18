50428b83b3f674324ecd5e22f2433e73e354bac3
Alena Gritsiuk <alena22292@mail.ru>
2020-11-18 12:15:14 +0100

    Create 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts 'Adding restaurants...'
+
+taco_bell = {
+  name: 'Taco Bell',
+  address: '21 Main Way',
+  category: "chinese",
+  phone_number: '67792940'
+}
+mac_bell = {
+  name: 'Mac_bell',
+  address: '22 Main Way',
+  category: "chinese",
+  phone_number: '67792940'
+}
+turbo_bell = {
+  name: 'Turbo Bell',
+  address: '23 Main Way',
+  category: "italian",
+  phone_number: '67792940'
+}
+taco_bulls = {
+  name: 'Taco Bulls',
+  address: '24 Main Way',
+  category: "chinese",
+  phone_number: '67792940'
+}
+time_eat = {
+  name: 'Time eat',
+  address: '25 Main Way',
+  category: "chinese",
+  phone_number: '67792940'
+}
+
+Restaurant.create!(taco_bell)
+Restaurant.create!(mac_bell)
+Restaurant.create!(turbo_bell)
+Restaurant.create!(taco_bulls)
+Restaurant.create!(time_eat)
+
+puts 'Done.'
